449d797b34e8c00bdff639e1e58e8ada3a45ebc6
Vanza Setia <venusbumi2@gmail.com>
2026-06-15 21:01:10 +0700

    Make Vitamin L3 page
    I start developing Vitamin L3 as a way for me to share links to people.

---
+++ src/vl3/capsules/capsules.11tydata.js
@@ -0,0 +1,12 @@
+export default {
+  layout: "layouts/vl3-capsule.njk",
+  tags: ["vl3"],
+  permalink: "/vitamin-l3/{{ page.fileSlug }}/index.html",
+  author: {
+    name: "Vanza Setia"
+  },
+  copyright: {
+    holder: "Vanza Setia"
+  },
+  capsule: true
+};
---
+++ src/vl3/vl3.11tydata.js
@@ -0,0 +1,3 @@
+export default {
+  layout: "layouts/vl3.njk"
+};
