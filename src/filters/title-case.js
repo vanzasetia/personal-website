869a9a2628b085de89ac163dadee789b74ea0c8e
Vanza Setia <venusbumi2@gmail.com>
2026-07-01 14:58:38 +1000

    Add 'UEFI' to the title case exceptions

--- src/filters/title-case.js
+++ src/filters/title-case.js
@@ -26,7 +26,7 @@ import titleCase from "title";
 
 export default function (title) {
   const exceptions = {
-    special: ["AI", "LLMS", "StackOverflow", "SSH"]
+    special: ["AI", "LLMS", "StackOverflow", "SSH", "UEFI"]
   };
   return titleCase(title, exceptions);
 }
